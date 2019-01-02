99683b3373fbf709714330d50c0f3b9e545f25bc
maaonan <xiaozhiqiu13@sina.com>
2019-01-02 23:34:57 +0800

    Use SQLiteDatabase API Update in database with conditions.

--- app/src/main/java/reagodjj/example/com/sqlitestudent/SecondActivity.java
+++ app/src/main/java/reagodjj/example/com/sqlitestudent/SecondActivity.java
@@ -71,7 +71,9 @@ public class SecondActivity extends AppCompatActivity {
         rgGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == R.id.rb_male || checkedId == R.id.rb_female)
+                if (checkedId == R.id.rb_male)
+                    gender = rbMale.getText().toString();
+                else if (checkedId == R.id.rb_female)
                     gender = rbFemale.getText().toString();
             }
         });
@@ -80,7 +82,7 @@ public class SecondActivity extends AppCompatActivity {
     public void operate(View view) {
         String name = etName.getText().toString();
         String age = etAge.getText().toString();
-        final String condition_id = etNumber.getText().toString();
+//        final String condition_id = etNumber.getText().toString();
         switch (view.getId()) {
             case R.id.bt_insert:
                 //在SqliteDatabase类下，提供四个方法
@@ -104,15 +106,27 @@ public class SecondActivity extends AppCompatActivity {
 //                参数1：表名
 //                参数2：条件列（“_id=? and name = ?”）
 //                参数3：条件值（new String[]{}）
-                int count = sqLiteDatabase.delete("info_student", "age<?", new String[]{age});
+                int count;
+                if (!age.equals("")) {
+                    //统计影响数据的行数
+                    count = sqLiteDatabase.delete("info_student", "age<?", new String[]{age});
+                } else {
+                    count = sqLiteDatabase.delete("info_student", null, null);
+                }
                 if (count > 0)
                     Toast.makeText(SecondActivity.this, R.string.delete_success, Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.bt_update:
-                String update = "update info_student set name = ?, age = ?, gender = ? where _id = ?";
-                sqLiteDatabase.execSQL(update, new String[]{name, age, gender, condition_id});
-                Toast.makeText(SecondActivity.this, R.string.update_success, Toast.LENGTH_SHORT).show();
+                ContentValues values_1 = new ContentValues();
+                //insert into 表明(列1，列2) values（值1，值2）
+                values_1.put("name", name);
+                values_1.put("age", age);
+                values_1.put("gender", gender);
+                int count_1 = sqLiteDatabase.update("info_student", values_1,
+                        "name like ?", new String[]{name});
+                if (count_1 > 0)
+                    Toast.makeText(SecondActivity.this, R.string.update_success, Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.bt_select:
@@ -124,9 +138,14 @@ public class SecondActivity extends AppCompatActivity {
 //            参数5：分组（不需要传入null） group by
 //            参数6：去除不符合条件的组（不需要传入null） having
 //            参数7：排序（不需要传入null） order by
-                Cursor cursor = sqLiteDatabase.query("info_student", new String[]{"_id, name, age, gender"},
-                        "age>?", new String[]{age}, null, null, "age desc");
-
+                Cursor cursor;
+                if (!age.equals("")) {
+                    cursor = sqLiteDatabase.query("info_student", new String[]{"_id, name, age, gender"},
+                            "age>?", new String[]{age}, null, null, "age desc");
+                } else {
+                    cursor = sqLiteDatabase.query("info_student", null,
+                            null, null, null, null, "age desc");
+                }
                 SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this,
                         R.layout.select_item, cursor, new String[]{"_id", "name", "age", "gender"},
                         new int[]{R.id.tv_id, R.id.tv_name, R.id.tv_age, R.id.tv_gender},
