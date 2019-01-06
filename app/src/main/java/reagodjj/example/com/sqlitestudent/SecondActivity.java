e8717e5c757eb62eac942db6bd77bdc5cc5dfe09
maaonan <xiaozhiqiu13@sina.com>
2019-01-06 15:53:46 +0800

    Use StudentDao to package insert and select.

--- app/src/main/java/reagodjj/example/com/sqlitestudent/SecondActivity.java
+++ app/src/main/java/reagodjj/example/com/sqlitestudent/SecondActivity.java
@@ -1,11 +1,8 @@
 package reagodjj.example.com.sqlitestudent;
 
 import android.content.ContentValues;
-import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +14,9 @@ import android.widget.RadioGroup;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
+import reagodjj.example.com.sqlitestudent.dao.StudentDao;
+import reagodjj.example.com.sqlitestudent.entity.Student;
+
 public class SecondActivity extends AppCompatActivity {
 
     private EditText etName;
@@ -27,6 +27,7 @@ public class SecondActivity extends AppCompatActivity {
     private EditText etNumber;
     private ListView lvSelectItem;
 
+    private StudentDao studentDao;
     private SQLiteDatabase sqLiteDatabase;
     private String gender = "男";
 
@@ -39,24 +40,7 @@ public class SecondActivity extends AppCompatActivity {
 
 //        String path = Environment.getExternalStorageDirectory() + "/student.db";
 
-        SQLiteOpenHelper sqLiteOpenHelper = new SQLiteOpenHelper(this, "student.db", null, 1) {
-            @Override
-            public void onCreate(SQLiteDatabase db) {
-                Toast.makeText(SecondActivity.this, R.string.add_database, Toast.LENGTH_SHORT).show();
-
-                String createTable = "create table info_student (_id integer primary key autoincrement,"
-                        + "name varchar(20), age integer, gender varchar(2))";
-                db.execSQL(createTable);
-                Toast.makeText(SecondActivity.this, R.string.add_chart, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-                Toast.makeText(SecondActivity.this, R.string.database_update, Toast.LENGTH_SHORT).show();
-            }
-        };
-
-        sqLiteDatabase = sqLiteOpenHelper.getReadableDatabase();
+        studentDao = new StudentDao(this);
     }
 
     private void initView() {
@@ -82,23 +66,13 @@ public class SecondActivity extends AppCompatActivity {
     public void operate(View view) {
         String name = etName.getText().toString();
         String age = etAge.getText().toString();
-//        final String condition_id = etNumber.getText().toString();
+        String condition_id = etNumber.getText().toString();
         switch (view.getId()) {
             case R.id.bt_insert:
-                //在SqliteDatabase类下，提供四个方法
-                //insert（添加）、delete（删除）、update（修改）、query（查询）
-                //都不需要写sql语句
-                //参数1：你所要操作的数据库表的名称
-                //参数2：可以为空的列.  如果第三个参数是null或者说里面没有数据
-                //那么我们的sql语句就会变为insert into info_tb () values ()  ，在语法上就是错误的
-                //此时通过参数3指定一个可以为空的列，语句就变成了insert into info_tb (可空列) values （null）
-                ContentValues values = new ContentValues();
-                //insert into 表明(列1，列2) values（值1，值2）
-                values.put("name", name);
-                values.put("age", age);
-                values.put("gender", gender);
-                long id = sqLiteDatabase.insert("info_student", null, values);
-                Toast.makeText(this, getString(R.string.add_student_id, id), Toast.LENGTH_SHORT).show();
+                if (!age.equals("")) {
+                    Student student = new Student(name, Integer.parseInt(age), gender);
+                    studentDao.addStudent(student);
+                }
                 break;
 
             case R.id.bt_delete:
@@ -130,22 +104,26 @@ public class SecondActivity extends AppCompatActivity {
                 break;
 
             case R.id.bt_select:
-//                query:
-//                参数1：表名
-//                参数2：查询列（String数组），查询所有传入null或{“*”}
-//            参数3：条件列（不需要传入null） “列1=？and 列2 =？”
-//            参数4：条件值（不需要传入null） （数组）
-//            参数5：分组（不需要传入null） group by
-//            参数6：去除不符合条件的组（不需要传入null） having
-//            参数7：排序（不需要传入null） order by
-                Cursor cursor;
+                String key = "";
+                String value = "";
                 if (!age.equals("")) {
-                    cursor = sqLiteDatabase.query("info_student", new String[]{"_id, name, age, gender"},
-                            "age>?", new String[]{age}, null, null, "age desc");
-                } else {
-                    cursor = sqLiteDatabase.query("info_student", null,
-                            null, null, null, null, "age desc");
+                    key = "age";
+                    value = age;
+                } else if (!condition_id.equals("")) {
+                    key = "_id";
+                    value = condition_id;
+                } else if (!name.equals("")) {
+                    key = "name";
+                    value = name;
                 }
+
+                Cursor cursor;
+                if (key.equals(""))
+                    cursor = studentDao.selectStudent();
+                else {
+                    cursor = studentDao.selectStudent(key, value);
+                }
+
                 SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this,
                         R.layout.select_item, cursor, new String[]{"_id", "name", "age", "gender"},
                         new int[]{R.id.tv_id, R.id.tv_name, R.id.tv_age, R.id.tv_gender},
---
+++ app/src/main/java/reagodjj/example/com/sqlitestudent/dao/StudentDao.java
@@ -0,0 +1,84 @@
+package reagodjj.example.com.sqlitestudent.dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import reagodjj.example.com.sqlitestudent.R;
+import reagodjj.example.com.sqlitestudent.entity.Student;
+
+public class StudentDao {
+    private SQLiteDatabase sqLiteDatabase;
+    private Context context;
+
+    public StudentDao(final Context context) {
+        this.context = context;
+        SQLiteOpenHelper sqLiteOpenHelper = new SQLiteOpenHelper(context, "student.db", null, 1) {
+            @Override
+            public void onCreate(SQLiteDatabase db) {
+                Toast.makeText(context, R.string.add_database, Toast.LENGTH_SHORT).show();
+
+                String createTable = "create table info_student (_id integer primary key autoincrement,"
+                        + "name varchar(20), age integer, gender varchar(2))";
+                db.execSQL(createTable);
+                Toast.makeText(context, R.string.add_chart, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+                Toast.makeText(context, R.string.database_update, Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        sqLiteDatabase = sqLiteOpenHelper.getReadableDatabase();
+    }
+
+    public void addStudent(Student student) {
+        //在SqliteDatabase类下，提供四个方法
+        //insert（添加）、delete（删除）、update（修改）、query（查询）
+        //都不需要写sql语句
+        //参数1：你所要操作的数据库表的名称
+        //参数2：可以为空的列.  如果第三个参数是null或者说里面没有数据
+        //那么我们的sql语句就会变为insert into info_tb () values ()  ，在语法上就是错误的
+        //此时通过参数3指定一个可以为空的列，语句就变成了insert into info_tb (可空列) values （null）
+        ContentValues values = new ContentValues();
+        //insert into 表明(列1，列2) values（值1，值2）
+        values.put("name", student.getName());
+        values.put("age", student.getAge());
+        values.put("gender", student.getGender());
+        long id = sqLiteDatabase.insert("info_student", null, values);
+        Toast.makeText(context, context.getString(R.string.add_student_id, id), Toast.LENGTH_SHORT).show();
+    }
+
+    public Cursor selectStudent(String... strs) {
+//        //1.查询所有(没有参数)
+//        String sql = "select * from info_tb ";
+//        //2.含条件查询（姓名/年龄/编号）（参数形式：第一个参数指明条件，第二个参数指明条件值）
+//        if(strs.length != 0){
+//            sql += " where " + strs[0] + "='" + strs[1] + "'";
+//        }
+//        Cursor c = db.rawQuery(sql,null);
+//        return c;
+
+        Cursor cursor;
+        if (strs.length != 0) {
+            cursor = sqLiteDatabase.query("info_student", new String[]{"_id, name, age, gender"},
+                    strs[0] + "= ?", new String[]{strs[1]}, null, null, "age desc");
+        } else {
+            cursor = sqLiteDatabase.query("info_student", null,
+                    null, null, null, null, "age desc");
+        }
+        return cursor;
+    }
+
+    public void updateStudent() {
+
+    }
+
+    public void deleteStudent() {
+
+    }
+}
